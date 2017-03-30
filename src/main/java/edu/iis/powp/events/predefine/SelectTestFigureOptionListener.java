e0a39edeeabec3ab706fe719fcc06f844a5346ab
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-03-31 00:44:56 +0200

    3.2.3

--- src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
@@ -9,10 +9,19 @@ import edu.iis.powp.app.DriverManager;
 
 public class SelectTestFigureOptionListener implements ActionListener
 {
+	String script = "Figure Joe 1";
+	
+    public SelectTestFigureOptionListener(String script) {
+		super();
+		this.script = script;
+	}
 
-    @Override
+	@Override
     public void actionPerformed(ActionEvent e)
     {
-        FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
+		if(script.equals("Figure Joe 1"))
+			FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
+		else if(script.equals("Figure Joe 2"))
+			FiguresJoe.figureScript2(Application.getComponent(DriverManager.class).getCurrentPlotter());
     }
 }
