dcb058e94394577feef48e7c91196d5275a4dc63
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-04-06 10:56:46 +0200

    3.3.1 LinePlotterAdapter

---
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -0,0 +1,54 @@
+package edu.iis.powp.adapter;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.app.Context;
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.ILine;
+import edu.kis.powp.drawer.shape.LineFactory;
+
+
+/**
+ * Plotter adapter to drawer with several bugs. 
+ */
+public class LinePlotterAdapter extends DrawPanelController implements IPlotter
+{ 
+	private int startX = 0, startY = 0;
+	private DrawPanelController controller;
+	private ILine line;
+	
+    public LinePlotterAdapter(Context context) {
+		super();
+		this.initialize(context.getFreePanel());
+	}
+    
+	public LinePlotterAdapter(DrawPanelController controller, ILine iLine) {
+		super();
+		this.controller = controller;
+		this.line = iLine;
+	}
+
+	@Override
+    public void setPosition(int x, int y)
+    {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void drawTo(int x, int y)
+    {
+    	line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);        
+        
+        controller.drawLine(line);		
+		setPosition(x,y);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "LineAdapter";
+    }
+}
+
+//3.2.2 Przetestowane - rysuje w odpowiednim oknie koperte
