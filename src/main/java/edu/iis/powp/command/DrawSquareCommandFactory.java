495809dfeb8b70fc525c8e118359650f5e3436c3
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-04-27 14:52:34 +0200

    Lab4 3.1.4

---
+++ src/main/java/edu/iis/powp/command/DrawSquareCommandFactory.java
@@ -0,0 +1,14 @@
+package edu.iis.powp.command;
+
+public class DrawSquareCommandFactory {
+	public ComplexCommand create() {
+		ComplexCommand complexCommand = new ComplexCommand();
+		complexCommand.add(new CommandSetPosition(0,0));
+		complexCommand.add(new CommandDrawLineToPosition(30,0));
+		complexCommand.add(new CommandDrawLineToPosition(30,30));
+		complexCommand.add(new CommandDrawLineToPosition(0,30));
+		complexCommand.add(new CommandDrawLineToPosition(0,0));
+		
+		return complexCommand;
+	}
+}
